34a25ddc205f214bf346109e5244ceef61a62e4e
Rochety <Rochety@DESKTOP-AVMJ5D9>
2020-05-11 01:54:41 +0200

    Finish

--- Parcial2Entidad/RegistrarEntidadesGui.Designer.cs
+++ Parcial2Entidad/RegistrarEntidadesGui.Designer.cs
@@ -37,6 +37,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.TipoCombo = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label1
@@ -76,14 +77,14 @@
             // 
             // NumeroRegistrotxt
             // 
-            this.NumeroRegistrotxt.Location = new System.Drawing.Point(181, 142);
+            this.NumeroRegistrotxt.Location = new System.Drawing.Point(167, 142);
             this.NumeroRegistrotxt.Name = "NumeroRegistrotxt";
             this.NumeroRegistrotxt.Size = new System.Drawing.Size(100, 20);
             this.NumeroRegistrotxt.TabIndex = 8;
             // 
             // DtFecha
             // 
-            this.DtFecha.Location = new System.Drawing.Point(181, 82);
+            this.DtFecha.Location = new System.Drawing.Point(167, 82);
             this.DtFecha.Name = "DtFecha";
             this.DtFecha.Size = new System.Drawing.Size(200, 20);
             this.DtFecha.TabIndex = 9;
@@ -119,11 +120,20 @@
             this.TipoCombo.Size = new System.Drawing.Size(121, 21);
             this.TipoCombo.TabIndex = 12;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel1.Location = new System.Drawing.Point(426, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 422);
+            this.panel1.TabIndex = 13;
+            // 
             // RegistrarEntidadesGui
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 409);
+            this.ClientSize = new System.Drawing.Size(627, 419);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.TipoCombo);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
@@ -151,5 +161,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox TipoCombo;
+        private System.Windows.Forms.Panel panel1;
     }
 }
